fd67f658d741212256cbd206ba2013e5f3305378
SubRosa <63234606+SubRosa4819@users.noreply.github.com>
2020-11-25 15:14:14 +0100

    qsd
    qsd

---
+++ src/Scripts/Panier.js
@@ -0,0 +1,22 @@
+class Panier {
+    constructor() {
+        this.produits = [];
+        this.total = 0;
+    }
+
+    AddToPanier(produit) {
+        this.produits.Add(produit);        
+    }
+
+    DeleteFromPanier(produit) {
+        let prd = this.produits.find(p => p === produit)
+        this.produits.splice(prd, 0);
+    }
+
+    getTotal() {
+        this.produits.forEach(p => {
+                this.total += p.prix
+            }
+        )
+    }
+}
