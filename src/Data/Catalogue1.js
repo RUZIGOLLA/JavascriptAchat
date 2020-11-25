f5a4557878f1b7fc2eeb0c8d838d1e7b557c0053
SubRosa <63234606+SubRosa4819@users.noreply.github.com>
2020-11-25 14:46:36 +0100

    Catalog

---
+++ src/Data/Catalogue1.js
@@ -0,0 +1,39 @@
+
+var catalog = [ 
+    {
+        "name": "Nounours",
+        "prix": "15",
+        "nombre": "0",
+    },
+    {
+        "name": "Chemise",
+        "prix": "35",
+        "nombre": "0",
+    }
+    ,    {
+        "name": "Coussin",
+        "prix": "25",
+        "nombre": "0",
+    },    {
+        "name": "Draps",
+        "prix": "35",
+        "nombre": "0",
+    },
+        {
+        "name": "Edredon",
+        "prix": "25",
+        "nombre": "0",
+    },    {
+        "name": "Nounours",
+        "prix": "15",
+        "nombre": "0",
+    },    {
+        "name": "Nounours",
+        "prix": "15",
+        "nombre": "0",
+    },    {
+        "name": "Nounours",
+        "prix": "15",
+        "nombre": "0",
+    }
+]
