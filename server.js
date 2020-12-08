dc02d819f5ea5e226beed2f461fc133348ce699a
SubRosa <63234606+SubRosa4819@users.noreply.github.com>
2020-12-08 10:36:15 +0100

    server.js
    server

---
+++ server.js
@@ -0,0 +1,14 @@
+// import { Panier } from './Panier.mjs';
+// import { Catalog } from './Catalog.mjs';
+
+const http = require("http");
+
+const server = http.createServer(function(req, res) {
+
+// A l’arrivée d’une requête
+res.write("Hello World!");
+res.end();
+})
+
+// Lancer le serveur
+server.listen(3000)
