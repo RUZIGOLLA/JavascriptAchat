315eafb0d862153fcaf1fa953f5fdc3a4f0ec44b
Jolan LARGETEAU <jolanlargeteau@gmail.com>
2020-12-08 11:27:12 +0100

    ajax import catalogue

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -1,7 +1,6 @@
-$(document).ready(() => {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    $('#app').html(`<p>You have been here for ${seconds} seconds.</p>`)
-  }, 1000);
+$.ajax({
+  url: "./Data/Catalogue1.js",
+  success: function (data){
+    console.log(data)
+  }
 })
