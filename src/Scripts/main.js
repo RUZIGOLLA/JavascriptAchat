93cc985bc6a9f87190ff1c82fef938ede412eac7
SubRosa <63234606+SubRosa4819@users.noreply.github.com>
2020-12-09 17:09:23 +0100

    qsd
    qd

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -35,10 +35,10 @@ $(document).ready(() => {
         <td>${product.price}</td>
         <td>${product.quantity}</td>
         <td>${product.price *product.quantity}€</td>
-        <tdonclick="del(${product.id})"><svgwidth="1em"height="1em"viewBox="001616"class="bibi-trash"fill="currentColor"xmlns="http://www.w3.org/2000/svg">
-        <pathd="M5.55.5A.5.500166v6a.5.5001-10V6a.5.5001.5-.5zm2.50a.5.5001.5.5v6a.5.5001-10V6a.5.5001.5-.5zm3.5a.5.5000-10v6a.5.500010V6z"/>
-        <pathfill-rule="evenodd"d="M14.53a11001-11H13v9a22001-22H5a22001-2-2V4h-.5a11001-1-1V2a110011-1H6a110011-1h2a1100111h3.5a1100111v1zM4.1184L44.059V13a1100011h6a110001-1V4.059L11.8824H4.118zM2.53V2h11v1h-11z"/>
-        </svg></td>
+        <td onclick="removeFromBasket(${product.id})"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+      </svg></td>
         </tr>
         `
         )
