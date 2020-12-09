f25caa90cdcddfa362e41ad00a596675facc536a
SubRosa <63234606+SubRosa4819@users.noreply.github.com>
2020-12-09 23:22:26 +0100

    search done
    il manque un petit bug d'affichage à régler quand il n'ya qu'un produit dans la liste

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -3,11 +3,14 @@ $(document).ready(() => {
     $.getScript('Scripts/Panier.js', () => {
       $.getScript('Data/Catalogue1.js', () => {
         $.get('productInPanier.html', (productInPanierString) => {
-
+          $('[data-toggle="popover"]').popover()
+          $('#site-search').on('input', e => searchInCatalog($('#site-search')[0].value))
+          this.constlistProduit = []
           this.listProduit = []
           let id = 1
           catalog.forEach(product => {
             listProduit.push(new Produit(id, product.name, product.description, product.image, parseFloat(product.price, 10)))
+            constlistProduit.push(new Produit(id, product.name, product.description, product.image, parseFloat(product.price, 10)))
             
             id++;
           })
@@ -28,7 +31,7 @@ $(document).ready(() => {
       $('#panierList').html('');
       panier.products.forEach((product)=>{
         let stringToAdd = this.productInPanierString
-        stringToAdd = stringToAdd.replace('${product.name}', product.name)
+        stringToAdd = listProduit.length > 0 ? stringToAdd.replace('${product.name}', product.name) : stringToAdd.replace('Aucun produit dans le panier', )
         stringToAdd = stringToAdd.replace('${product.price}', product.price)
         stringToAdd = stringToAdd.replace('${product.quantity}', product.quantity)
         stringToAdd = stringToAdd.replace('${product.id}', product.id)
@@ -38,7 +41,7 @@ $(document).ready(() => {
   }
     
   buy = (id) => {
-    let product = listProduit.find(product=>product.id==id)
+    let product = constlistProduit.find(product=>product.id==id)
     let quantityToAdd = parseInt($("#" + id + "-quantity").val())
     if (isNaN(quantityToAdd)) {
       quantityToAdd = 1;
@@ -57,21 +60,34 @@ $(document).ready(() => {
   // }
 
   removeFromBasket = (id) => {
-    let product = listProduit.find(product => product.id === id)
+    let product = constlistProduit.find(product => product.id === id)
     product.quantity = 0;
     panier.RemoveFromBasket(product);
     RefreshPanier();
   }
-
+  searchInCatalog = (searchString) => {
+          listProduit = []
+          constlistProduit.forEach(p => {
+          console.log(p.name.toLowerCase() === searchString.toLowerCase(), p, searchString)
+          if (p.name.toLowerCase().includes(searchString.toLowerCase()) === true) {
+              listProduit.push(p);
+          }
+         
+        })
+    //  }
+        populateHtml();
+  }
   populateHtml = () => {
+    $("#materiels").empty()
+    console.log($($("#materiels")[0]))
+    
     for (let i = 0; i < listProduit.length; i++) {
-      $(".materiels").append(`
-      <div class="mx-3 my-2  px-0 shadow-lg card col-2">
-      <div class="  " >
+      $("#materiels").append(`
+      <div class="mx-3 my-2  px-0 shadow-lg card col-xs-6 col-sm-2">
         <div class="card-title text-center pt-1 mb-0">
             <h4 class="mb-0">${listProduit[i].name}</h4>
         </div>
-        <div class="card-body pt-0">
+        <div class="card-body imgs pt-0" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">
             <img src="${listProduit[i].image}" class="card-img-top">
         </div>
         <div class="card-footer px-1">
@@ -82,7 +98,6 @@ $(document).ready(() => {
               <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
             </svg>
           </button>
-        </div>
       </div></div>`)
     }
   }
