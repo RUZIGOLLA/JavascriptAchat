f373607604619f223aaa77ce8ebe151f48947da6
Jolan LARGETEAU <jolanlargeteau@gmail.com>
2020-12-09 18:14:29 +0100

    FIX: Block buy negative quantity of product

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -43,9 +43,11 @@ $(document).ready(() => {
     if (isNaN(quantityToAdd)) {
       quantityToAdd = 1;
     }
-    product.quantity = quantityToAdd + product.quantity > 10 ? 10 : quantityToAdd + product.quantity
-    panier.AddToBasket(product)
-    RefreshPanier()
+    if (quantityToAdd > 0) {
+      product.quantity = quantityToAdd + product.quantity > 10 ? 10 : quantityToAdd + product.quantity
+      panier.AddToBasket(product)
+      RefreshPanier()
+    }
   }
       
   // buy = (id) => {
@@ -74,7 +76,7 @@ $(document).ready(() => {
         <div class="card-footer px-1">
           <div>Prix : ${listProduit[i].price} €</div>
           <input id="${listProduit[i].id}-quantity" class="form-control" type="number" placeholder="Quantité"/>
-          <button type="button" onclick="buy(${listProduit[i].id})" class="btn btn-outline-danger btn-block">
+          <button type="button" onclick="buy(${listProduit[i].id})" class="btn btn-outline-success btn-block mt-1">
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
             </svg>
