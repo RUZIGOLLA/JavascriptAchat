2834ccf8f25a48918b9a7e06c48a1644b95a99fe
Jolan LARGETEAU <jolanlargeteau@gmail.com>
2021-01-13 09:45:53 +0100

    disabled and enable buy button

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -11,14 +11,14 @@ $(document).ready(() => {
           catalog.forEach(product => {
             listProduit.push(new Produit(id, product.name, product.description, product.image, parseFloat(product.price, 10)))
             constlistProduit.push(new Produit(id, product.name, product.description, product.image, parseFloat(product.price, 10)))
-            
+
             id++;
           })
           populateHtml()
           this.productInPanierString = productInPanierString
           this.panier = new Panier()
           await animateHtml()
-    
+
         })
       })
     })
@@ -27,14 +27,14 @@ $(document).ready(() => {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   async function animateHtml() {
-   let prdts = $(".produits") 
+   let prdts = $(".produits")
    $(prdts).addClass("produit")
    // console.log(produits.length)
    for (let i = 0; i < prdts.length; i++) {
     console.log(prdts[i]);
       // $(prdts[i]).addClass("produit")
       await sleep(200).then(() => addLoad(prdts[i]))
-   } 
+   }
     }
     async function removeLoad(prdt) {
       $(prdt).removeClass('load')
@@ -58,7 +58,7 @@ $(document).ready(() => {
         $('#panierList').append(stringToAdd)
       })
   }
-    
+
   buy = (id) => {
     let product = constlistProduit.find(product=>product.id==id)
     let quantityToAdd = parseInt($("#" + id + "-quantity").val())
@@ -71,7 +71,7 @@ $(document).ready(() => {
       RefreshPanier()
     }
   }
-      
+
   // buy = (id) => {
   //   let product = listProduit.find(product => product.id === id)
   //   panier.AddToBasket(product)
@@ -91,7 +91,7 @@ $(document).ready(() => {
           if (p.name.toLowerCase().includes(searchString.toLowerCase()) === true) {
               listProduit.push(p);
           }
-         
+
         })
    $(".produits").removeClass("produit")
    //  }
@@ -101,7 +101,7 @@ $(document).ready(() => {
   }
   populateHtml = () => {
     $("#materiels").empty()
-   
+
     if (listProduit.length == 0) {
       $("#materiels").append(`<div class="text-center"><h4> Aucun produit trouvé ...</h4></div>`)
 
@@ -117,14 +117,26 @@ $(document).ready(() => {
         </div>
         <div class="card-footer px-1">
           <div>Prix : ${listProduit[i].price} €</div>
-          <input id="${listProduit[i].id}-quantity" class="form-control" type="number" placeholder="Quantité"/>
-          <button type="button" onclick="buy(${listProduit[i].id})" class="btn btn-outline-success btn-block mt-1">
+          <input id="${listProduit[i].id}-quantity" class="form-control" type="number" placeholder="Quantité" onchange="changeQuantity(this)"/>
+          <button id="${listProduit[i].id}-buy" type="button" onclick="buy(${listProduit[i].id})" class="btn btn-outline-success btn-block mt-1 inactive" disabled>
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
             </svg>
           </button>
       </div></div>`)
     }
+  }
+
+  changeQuantity = (element) => {
+      if (element.value !== "0" && element.value !== "") {
+          let buyButton = element.id.split('-')[0] + '-buy'
+          $('#'+buyButton).removeClass('inactive');
+          document.getElementById(buyButton).removeAttribute('disabled')
+      } else {
+          let buyButton = element.id.split('-')[0] + '-buy'
+          $('#'+buyButton).addClass('inactive');
+          document.getElementById(buyButton).setAttribute('disabled', 'disabled')
+      }
   }
 
 })
