674d5e8f625979fef5e99873ab70f833f8cddb97
SubRosa <63234606+SubRosa4819@users.noreply.github.com>
2021-01-13 09:24:08 +0100

    yo el rapo

--- src/Data/Catalogue1.js
+++ src/Data/Catalogue1.js
@@ -9,41 +9,41 @@ var catalog = [
         "description": "Salut",
         "name": "Chemise",
         "price": "35",
-        "image": "Data/images/nounours.jpg"
+        "image": "Data/images/chemise.jpg"
     },
     {
         "description": "Salut",
         "name": "Coussin",
         "price": "25",
-        "image": "Data/images/nounours.jpg"
+        "image": "Data/images/coussin.png"
     },
     {
         "description": "Salut",
         "name": "Draps",
         "price": "35",
-        "image": "Data/images/nounours.jpg"
+        "image": "Data/images/draps.jpg"
     },
     {
         "description": "Salut",
         "name": "Edredon",
         "price": "25",
-        "image": "Data/images/nounours.jpg"
+        "image": "Data/images/Edredon.jpg"
     },
     {
         "description": "Salut",
-        "name": "Nounours",
+        "name": "Nounours 2",
+        "price": "15",
+        "image": "Data/images/nounours.png"
+    },
+    {
+        "description": "Salut",
+        "name": "Nounours 3",
         "price": "15",
         "image": "Data/images/nounours.jpg"
     },
     {
         "description": "Salut",
-        "name": "Nounours",
-        "price": "15",
-        "image": "Data/images/nounours.jpg"
-    },
-    {
-        "description": "Salut",
-        "name": "Nounours",
+        "name": "Nounours 4",
         "price": "15",
         "image": "Data/images/nounours.jpg"
     }
